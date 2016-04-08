c86f47e6cb5ced32f95c624c67c978f95cbfed57
kowshik777 <kowshik16.kk@gmail.com>
2016-04-08 14:44:12 +0530

    make it better

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Workspace
   class Application < Rails::Application
     config.assets.paths << 'path_to_images'
+    config.assets.precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
